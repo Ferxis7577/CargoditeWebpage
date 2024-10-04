3c046d3f4b1f2d44ae23238ed4da73b0c7cf6f69
Alexis <alexis6132@gmail.com>
2024-10-04 13:38:55 -0500

    contact email form

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+  // script.js
+
+    // Add your access key dynamically to the form
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('access-key').value = '82f2c2ba-06f3-475f-8d9b-02105f45ec8a';
+    }); 
